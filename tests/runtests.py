e25ea35fc42a338a36a64181032a9ca7f736e34f
Matt Newville <newville@cars.uchicago.edu>
2015-12-05 20:04:46 -0600

    simplified tests

--- tests/runtests.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-pref = sys.prefix + '/lib/python2.6/site-packages'
-print pref
-os.system('coverage run --source=asteval unittest_1.py')
-os.system("coverage report")
-os.system("coverage html")
