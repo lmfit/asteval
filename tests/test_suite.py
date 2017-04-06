278b843a0ca684a73a5c9c4532f0f9803be4b1dd
Don Welch <dwelch91@gmail.com>
2017-04-06 11:10:16 -0700

    Try again.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -26,7 +26,7 @@ else:
     # noinspection PyUnresolvedReferences
     from cStringIO import StringIO
 
-from asteval.asteval import NameFinder, Interpreter
+from ..asteval import NameFinder, Interpreter
 
 
 @contextlib.contextmanager
