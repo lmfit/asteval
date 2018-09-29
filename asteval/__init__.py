e388a003415cdebc910c3ac10bc5a395d10d9424
Matthew Newville <newville@cars.uchicago.edu>
2018-09-29 08:08:37 -0500

    version 0.9.13

--- asteval/__init__.py
+++ asteval/__init__.py
@@ -9,8 +9,8 @@
    Expressions can be compiled into ast node for later evaluation,
    using the values in the symbol table current at evaluation time.
 
-   version: 0.9.12
-   last update: 2018-March-8
+   version: 0.9.13
+   last update: 2018-Sept-29
    License:  MIT
    Author:  Matthew Newville <newville@cars.uchicago.edu>
             Center for Advanced Radiation Sources,
