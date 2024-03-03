c0d3a4c0aec116595880f38bb152c4994e828270
Matthew Newville <newville@cars.uchicago.edu>
2024-03-03 11:16:12 -0600

    remove date tag, unused decsciption from __init__.py

--- asteval/__init__.py
+++ asteval/__init__.py
@@ -1,27 +1,7 @@
-"""
-   ASTEVAL provides a numpy-aware, safe(ish) "eval" function
-
-   Emphasis is on mathematical expressions, and so numpy ufuncs
-   are used if available.  Symbols are held in the Interpreter
-   symbol table 'symtable':  a simple dictionary supporting a
-   simple, flat namespace.
-
-   Expressions can be compiled into ast node for later evaluation,
-   using the values in the symbol table current at evaluation time.
-
-   version: 0.9.31
-   last update: 2023-July-16
-   License:  MIT
-   Author:  Matthew Newville <newville@cars.uchicago.edu>
-            Center for Advanced Radiation Sources,
-            The University of Chicago
-"""
-
+from .version import version as __version__
 from .asteval import Interpreter
 from .astutils import (NameFinder, get_ast_names, make_symbol_table,
                        valid_symbol_name)
-
-from .version import version as __version__
 
 __all__ = ['Interpreter', 'NameFinder', 'valid_symbol_name',
            'make_symbol_table', 'get_ast_names', '__version__']
