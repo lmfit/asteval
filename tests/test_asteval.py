790521fe9fb47b60fb3d1eac79114ecbd4b2516f
Matthew Newville <newville@cars.uchicago.edu>
2024-06-30 10:09:49 -0500

    add a few more list comprehension examples

--- tests/test_asteval.py
+++ tests/test_asteval.py
@@ -739,6 +739,8 @@ def test_list_comprehension_more(nested):
                 '[p*2.5 for p in odd]',
                 '[n for p in zip(odd, even) for n in p]',
                 '[(i*i + 0.5) for i in range(4)]',
+                '[i*3.2 for i in odd if i > 6 and i < 18]',
+                '[i-1.0 for i in odd if i > 4 and i*2 not in (26, 34)]',
                 ]:
 
         interp(f"out = {expr}")
