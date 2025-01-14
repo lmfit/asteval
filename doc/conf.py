10c8fbf308e4ddb659a84c12e8a3a4e85c9f47ea
Matthew Newville <newville@cars.uchicago.edu>
2025-01-14 10:34:32 -0600

    doc config

--- doc/conf.py
+++ doc/conf.py
@@ -172,7 +172,7 @@ htmlhelp_basename = 'astevaldoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_paper_size = 'letter'
 
 # The font size ('10pt', '11pt' or '12pt').
 #latex_font_size = '10pt'
